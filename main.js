b31b805dfdbe4170a6bac728d169222b9d3989b2
gomszab <gombosszabolcs88@gmail.com>
2025-04-01 19:16:21 +0200

    tablazat hozzadasa sima

--- main.js
+++ main.js
@@ -7,6 +7,20 @@ const makeDiv = (className) => {
 const containerDiv = makeDiv('container');
 document.body.appendChild(containerDiv);
 const tableDiv = makeDiv('table');
+const tableSim = document.createElement('table');
+tableDiv.appendChild(tableSim);
+const tableHead = document.createElement('thead');
+tableSim.appendChild(tableHead);
+const tableHeadRow =  document.createElement('tr');
+tableHead.appendChild(tableHeadRow)
+const theadCells = ['név', 'születési dátum', 'irányítószám'];
+for(const cellContent of theadCells){
+    const thcell = document.createElement('th');
+    thcell.innerText = cellContent;
+    tableHeadRow.appendChild(thcell);
+}
+const tbody = document.createElement('tbody');
+tableSim.appendChild(tbody);
 
 const formDiv = makeDiv('form');
 
